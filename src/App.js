bc56dc950fb5d54316b363523071968722771745
Yisacc Aberham <yaberham@blackboxintelligence.com>
2022-05-25 17:54:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import User from './components/User/User'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const data = [
+      { firstName: 'John', lastName: 'Smath' },
+      { firstName: 'Paul', lastName: 'Smeth' },
+      { firstName: 'Cody', lastName: 'Smith' },
+      { firstName: 'Jordan', lastName: 'Smoth' },
+      { firstName: 'Jim', lastName: 'Smuth' },
+    ]
+
+    return (
+      <div className="App">
+        <div className="pdf">
+          {data.map((user) => (
+            <User user={user} key={user.lastName} />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
